e2d2234b7fcd6c5d0e46722086224e61851f2eaf
lipex360x <lipex360@gmail.com>
2022-06-28 19:18:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-ekyacz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-ekyacz)
---
+++ src/template/App/index.tsx
@@ -0,0 +1,22 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from '../../styles/global';
+import theme from '../../styles/theme';
+
+import * as S from './styles';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <S.Wrapper>
+        <div>
+          <h1>Vite Boilerplate</h1>
+          <p>Update this page in: src/templates/App/index.tsx</p>
+        </div>
+      </S.Wrapper>
+
+      <GlobalStyles />
+    </ThemeProvider>
+  );
+}
+
+export default App;

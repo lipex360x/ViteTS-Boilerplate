9de0e65abbe163fe62cd37a8a2f4998c7e9eddfc
Felipe Borges <lipex360@gmail.com>
2022-07-10 13:55:01 -0300

    plop added

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,13 @@
-import font from './_font';
+import border from './_border'
 import color from './_color';
+import font from './_font';
+import spacing from './_spacing';
+import transition from './_transition';
 
 export default {
-  font,
+  border,
   color,
+  font,
+  spacing,
+  transition
 } as const;

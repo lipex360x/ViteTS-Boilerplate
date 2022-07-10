b5e859940cdf3e7b1961d6a4b5f0f0c40913d584
Felipe Borges <lipex360@gmail.com>
2022-07-10 13:58:08 -0300

    theme updated

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,8 @@
 import border from './_border'
 import color from './_color';
 import font from './_font';
+import grid from './_grid';
+import layers from './_layers';
 import spacing from './_spacing';
 import transition from './_transition';
 
@@ -8,6 +10,8 @@ export default {
   border,
   color,
   font,
+  grid,
+  layers,
   spacing,
   transition
 } as const;

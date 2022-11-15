4f73bf60f8bb9821270643e506e0c747973c4ec8
Felipe Bambirra <lipex360@gmail.com>
2022-11-15 19:02:32 -0300

    plop updated

--- generators/plopfile.ts
+++ generators/plopfile.ts
@@ -2,8 +2,10 @@ import { NodePlopAPI } from 'plop'
 
 import components from './modules/components'
 import forms from './modules/forms'
+import pages from './modules/pages'
 
 export default function (plop: NodePlopAPI) {
   plop.setGenerator('Component', components)
   plop.setGenerator('Form', forms)
+  plop.setGenerator('Pages', pages)
 }

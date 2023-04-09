7459ce67c8a395f50b95b1e01cca7c255da45508
Felipe Borges <lipex360@gmail.com>
2023-04-09 18:42:13 -0300

    localstorage service updated

--- src/services/localStorage.service.ts
+++ src/services/localStorage.service.ts
@@ -1,6 +1,7 @@
 export const localStorageService = {
   setItem: (key: string, item: string | object) => {
     const value = typeof item === 'object' ? JSON.stringify(item) : item
+    
     return window.localStorage.setItem(key, value)
   },
 
@@ -8,7 +9,9 @@ export const localStorageService = {
     const value = window.localStorage.getItem(key)
 
     if (!value) return
+
     if (typeof value === 'object') return JSON.parse(value)
+    
     return value
   },
 
